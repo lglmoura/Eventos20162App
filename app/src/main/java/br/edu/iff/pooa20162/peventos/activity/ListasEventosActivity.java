14b2fa8f13b057c62973b782de10209d2d3514cf
Luiz Gustavo L Moura <lglmoura@gmail.com>
2017-03-06 17:32:34 -0300

    Criando Pacotes Activity, Model e Adapter

--- app/src/main/java/br/edu/iff/pooa20162/peventos/activity/ListasEventosActivity.java
+++ app/src/main/java/br/edu/iff/pooa20162/peventos/activity/ListasEventosActivity.java
@@ -1,4 +1,4 @@
-package br.edu.iff.pooa20162.peventos;
+package br.edu.iff.pooa20162.peventos.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import br.edu.iff.pooa20162.peventos.R;
 
 public class ListasEventosActivity extends AppCompatActivity {
 
