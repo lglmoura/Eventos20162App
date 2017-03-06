3ce9972d1796915061d36e44fcf2b385f4b2039b
Luiz Gustavo L Moura <lglmoura@gmail.com>
2017-03-06 17:46:21 -0300

    Criando Principal de Eventos

---
+++ app/src/main/java/br/edu/iff/pooa20162/peventos/activity/MainActivity.java
@@ -0,0 +1,21 @@
+package br.edu.iff.pooa20162.peventos.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import br.edu.iff.pooa20162.peventos.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+    public void chamaTelaEventos(View v){
+        Toast.makeText(this,"",Toast.LENGTH_SHORT);
+    }
+}
