d79943211d4efcf6f93964451ed5fec29d4dbd45
Luiz Gustavo Lourenço Moura <lglmoura@gmail.com>
2017-03-20 15:27:13 -0300

    Alterando a Classe LocalAdapter

--- app/src/main/java/br/edu/iff/pooa20162/peventos/adapter/LocalAdapter.java
+++ app/src/main/java/br/edu/iff/pooa20162/peventos/adapter/LocalAdapter.java
@@ -1,6 +1,8 @@
 package br.edu.iff.pooa20162.peventos.adapter;
 
 import android.content.Context;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +18,7 @@ import br.edu.iff.pooa20162.peventos.model.Local;
  * Created by aluno on 06/03/17.
  */
 
-public class LocalAdapter extends ArrayAdapter<Local> {
+public class LocalAdapter  extends ArrayAdapter<Local>  {
 
     private Context context = null;
     private ArrayList<Local> locais = null;
@@ -31,13 +33,22 @@ public class LocalAdapter extends ArrayAdapter<Local> {
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        View rowView = inflater.inflate(R.layout.linhavento, parent, false);
+        View rowView = inflater.inflate(R.layout.linhalocal, parent, false);
+        final Local item = getItem(position);
 
-        TextView nome = (TextView) rowView.findViewById(R.id.tvllvNome);
+        TextView nome = (TextView) rowView.findViewById(R.id.tvllvNomeLocal);
 
-        nome.setText(locais.get(position).getNome());
-        
+        //nome.setText(locais.get(position).getNome());
+        nome.setText(item.getNome());
+        rowView.setTag(item);
+
         return rowView;
+    }
+
+
+    @Override
+    public long getItemId(int position) {
+        return super.getItemId(position);
     }
 
 
